7ecba5d72ec7b3575355d2d1e24ee7e5cda2981e
“tosky2023” <panhequn007@gmail.com>
2023-03-07 23:40:20 +0800

    3.75

--- Memorize/AspectVGird.swift
+++ Memorize/AspectVGird.swift
@@ -16,18 +16,37 @@ struct AspectVGrid<Item,ItemView>: View where ItemView:View, Item:Identifiable {
     
     var body: some View {
         let width : CGFloat = 100
-        LazyVGrid(columns:[GridItem(.adaptive(minimum: width))]) {
+        LazyVGrid(columns:[adaptiveGridItem(width: width)],spacing: 0) {
             ForEach(items) { item in
                 content(item).aspectRatio(aspectRatio, contentMode: .fit)
             }
         }
     }
+    
+    private func adaptiveGridItem(width:CGFloat) -> GridItem {
+        var gridItem = GridItem(.adaptive(minimum: width))
+        gridItem.spacing = 0
+        return gridItem
+    }
+    
+    
+    
+    private func widthThatfits(itemCount: Int, in size: CGSize, itemAspectRatio: CGFloat) -> CGFloat {
+        var columnCount = 1
+        var rowCount = itemCount
+        repeat {
+            let itemWidth = size.width / CGFloat (columnCount)
+            let itemHeight = itemWidth / itemAspectRatio
+            if CGFloat(rowCount) * itemHeight < size.height {
+                break
+            }
+            columnCount += 1
+            rowCount = (itemCount + (columnCount - 1)) / columnCount
+        } while columnCount < itemCount
+        if columnCount > itemCount {
+            columnCount = itemCount
+            return floor(size.width / CGFloat (columnCount))
+        }
+        return floor(size.width / CGFloat (columnCount))
+    }
 }
-
-//
-//struct AspectVGird_Previews: PreviewProvider {
-//    static var previews: some View {
-//        AspectVGird()
-//    }
-//}
-//push test
