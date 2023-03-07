fb695abd7de3e3c8c61cd411b5c5a956fae64146
“tosky2023” <panhequn007@gmail.com>
2023-03-07 17:46:21 +0800

    3.7.2

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -14,7 +14,7 @@ struct EmojiMemoryGameView: View {
     
     var body: some View {
         //        ScrollView {
-        //            LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+        //            LazyVGrid(columns: [GridItem(.adaptive(minimum: 200))]) {
         //                ForEach(viewModel.cards) { card in
         //                    CardView(card: card)
         //                        .aspectRatio(2/3, contentMode: .fit)
@@ -41,19 +41,22 @@ struct CardView : View {
     
     let card: EmojiMemoryGame.Card
     
-    var body: some View{
-        ZStack{
-            let shape = RoundedRectangle(cornerRadius: 20)
-            if card.isFaceUp {
-                shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth:3)
-                Text(card.content).font(.largeTitle)
-            } else   if card.isMatched{
-                shape.opacity(0 )
-            } else {
-                shape.fill()
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack{
+                let shape = RoundedRectangle(cornerRadius: 20)
+                if card.isFaceUp {
+                    shape.fill().foregroundColor(.white)
+                    shape.stroke(lineWidth:3)
+                    Text(card.content).font(Font.system(size: min(geometry.size.width,geometry.size.height) * 0.9))
+                } else   if card.isMatched{
+                    shape.opacity(0 )
+                } else {
+                    shape.fill()
+                }
             }
         }
+        
     }
 }
 
