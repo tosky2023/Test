f1d2bd03493e18c9e1f104e0fb5bb208e3bb96ee
“tosky2023” <panhequn007@gmail.com>
2023-03-07 21:41:45 +0800

    3.7.3

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -13,19 +13,19 @@ struct EmojiMemoryGameView: View {
     @State var emojiCount:Int = 20
     
     var body: some View {
-        //        ScrollView {
-        //            LazyVGrid(columns: [GridItem(.adaptive(minimum: 200))]) {
-        //                ForEach(viewModel.cards) { card in
-        //                    CardView(card: card)
-        //                        .aspectRatio(2/3, contentMode: .fit)
-        //                        .onTapGesture {
-        //                            game.choose(card)
-        //                        }
-        //                }
-        //            }
-        //            .foregroundColor(.red)
-        //        }
-        //        .padding(.horizontal)
+    //        ScrollView {
+    //            LazyVGrid(columns: [GridItem(.adaptive(minimum: 200))]) {
+    //                ForEach(viewModel.cards) { card in
+    //                    CardView(card: card)
+    //                        .aspectRatio(2/3, contentMode: .fit)
+    //                        .onTapGesture {
+    //                            game.choose(card)
+    //                        }
+    //                }
+    //            }
+    //            .foregroundColor(.red)
+    //        }
+    //        .padding(.horizontal)
         AspectVGrid(items:viewModel.cards, aspectRatio:2/3, content: { card in
             CardView(card: card)
                 .aspectRatio(2/3, contentMode: .fit)
@@ -44,19 +44,30 @@ struct CardView : View {
     var body: some View {
         GeometryReader { geometry in
             ZStack{
-                let shape = RoundedRectangle(cornerRadius: 20)
+                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
                 if card.isFaceUp {
                     shape.fill().foregroundColor(.white)
-                    shape.stroke(lineWidth:3)
-                    Text(card.content).font(Font.system(size: min(geometry.size.width,geometry.size.height) * 0.9))
+                    shape.stroke(lineWidth: DrawingConstants.lineWidth)
+                    Text(card.content).font(font(in: geometry.size))
                 } else   if card.isMatched{
-                    shape.opacity(0 )
+                    shape.opacity(0)
                 } else {
                     shape.fill()
                 }
             }
         }
         
+    }
+    private func font(in size:CGSize) -> Font {
+       Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    }
+    
+    
+    
+    private struct DrawingConstants{
+        static let cornerRadius: CGFloat = 20
+        static let lineWidth: CGFloat = 3
+        static let fontScale: CGFloat = 0.8
     }
 }
 
